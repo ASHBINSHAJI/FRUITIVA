633ee3dcc574103a8b8f119bf96013d337ee77e9
Ashbin <ashbinshaji20088@gmail.com>
2026-06-23 15:57:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',function(){
+document.getElementById('ShopBtn').addEventListener('click', function(){
+    window.location.href='shop.html';
+});
+   document.getElementById('Tbtn').addEventListener('click',function(){
+      window.location.href='shop.html';
+    });
+});
